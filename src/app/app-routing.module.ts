f251022cfe86c0cdfa12934bea0a203235575635
Ioana <ioanalazar2008@yahoo.com>
2021-04-22 16:40:57 +0300

    Main page & register page components+routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [{ path: 'register', component: RegisterComponent }, {path: "", component: MainPageComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { RegisterComponent } from './register/register.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    RegisterComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
