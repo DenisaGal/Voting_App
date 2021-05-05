959556191e3aba99a32477d2d334d8cbbf37f416
Ioana <ioanalazar2008@yahoo.com>
2021-05-05 15:40:42 +0300

    FireAuth added

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
 import { LoginComponent } from '../login/login.component';
+import { AngularFireAuth } from "@angular/fire/auth";
 
 @Component({
   selector: 'app-register',
@@ -9,15 +10,27 @@ import { LoginComponent } from '../login/login.component';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor(public router: Router) { }
+  constructor(public router: Router, private auth: AngularFireAuth) { }
 
   ngOnInit(): void {
   }
 
-  register(): void{  //for now verific ca merge butonul si ia datele din field
-    let name = (<HTMLInputElement>document.getElementById("firstname")).value;
-    alert(name);
+  register(): void{
+
+    //gotta make a profile completion page and move this complex form there, while register is only  mail and pwd
+     let email = (<HTMLInputElement>document.getElementById("email")).value;
+     let password = (<HTMLInputElement>document.getElementById("password")).value;
+
+      this.auth.createUserWithEmailAndPassword(email, password)
+                .then((user) => {
+                  this.router.navigate(['./login']);
+                })
+                .catch((error) => {
+                  window.alert(error.message);
+                });
+      }
 
     //have to check AGE!!, maybe address dar nu cred ca reusim
+    //putem trimite e-mail de confirmare
+    //trebuie sa vedem cum encriptam datele
   }
-}
