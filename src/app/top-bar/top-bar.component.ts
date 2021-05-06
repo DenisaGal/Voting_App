8901bbe9d656c6e1cb5a1743cff91211cc78f0ea
Ioana <ioanalazar2008@yahoo.com>
2021-05-06 16:08:02 +0300

    Logged in different behaviour

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
 import { LoginComponent } from '../login/login.component';
+import { AngularFireAuth } from "@angular/fire/auth";
+import {ApplicationRef } from '@angular/core';
 
 @Component({
   selector: 'app-top-bar',
@@ -9,9 +11,39 @@ import { LoginComponent } from '../login/login.component';
 })
 export class TopBarComponent implements OnInit {
 
-  constructor( public router: Router) { }
+  public static isSignedIn:boolean = false;
+
+  constructor( private ref: ApplicationRef, private auth: AngularFireAuth, public router: Router ) {
+    let var1 = this.ref
+          this.auth.onAuthStateChanged(function(user) {
+            if (user) {
+              TopBarComponent.isSignedIn = true
+              var1.tick()
+              // User is signed in.
+            } else {
+              TopBarComponent.isSignedIn = false
+              var1.tick()
+
+              // No user is signed in.
+            }
+          });
+
+  }
 
   ngOnInit(): void {
+  }
+
+  getStatus(): boolean {
+     return TopBarComponent.isSignedIn;
+  }
+
+  logout(): void {
+    this.auth.signOut().then(() => {
+          TopBarComponent.isSignedIn = false;
+          this.router.navigate(['./']);
+        }).catch((error) => {
+          window.alert("Something went wrong :(");
+        });
   }
 
 }
