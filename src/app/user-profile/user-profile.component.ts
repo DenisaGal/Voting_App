0b8bdbed532c9624189e5b368d99184786708966
DenisaGal <denisagal09@yahoo.com>
2021-05-09 20:35:17 +0300

    Added Firestore Users database for text type data

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private db: AngularFirestore) { }
 
   ngOnInit(): void {
   }
 
-//basically dupa ce si fac cont, aici trebuie sa iti completezi profilul si abia dupa e ok ca user so now we have to see abt database cu user info
+	//basically dupa ce si fac cont, aici trebuie sa iti completezi profilul si abia dupa e ok ca user so now we have to see abt database cu user info
+
+	add_data(): void {
+		let iCNP = (<HTMLInputElement>document.getElementById("cnp")).value;
+		let iCID = (<HTMLInputElement>document.getElementById("serie")).value;
+		let iFirst_Name = (<HTMLInputElement>document.getElementById("firstname")).value;
+		let iLast_Name = (<HTMLInputElement>document.getElementById("lastname")).value;
+		let iCounty = (<HTMLInputElement>document.getElementById("county")).value;
+		let iCity = (<HTMLInputElement>document.getElementById("city")).value;
+		let iStreet = (<HTMLInputElement>document.getElementById("street")).value;
+		let iNumber = (<HTMLInputElement>document.getElementById("number")).value;
+		//let iGender = !!!!!!!
+
+		this.db.collection("Users").doc(iCNP).set({
+		    CID: iCID,
+		    First_Name: iFirst_Name,
+		    Last_Name: iLast_Name,
+		    Email: "",
+		    City: iCity,
+		    Street: iStreet,
+		    Number: iNumber,
+		    Gender: ""
+		})
+		.then(() => {
+    console.log("Document successfully written!");
+		})
+		.catch((error) => {
+		    console.error("Error writing document: ", error);
+		});
+	}
 }
