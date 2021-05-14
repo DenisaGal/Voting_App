1ad37b29974ba6346a83d61d9ea4b4c723494db1
DenisaGal <denisagal09@yahoo.com>
2021-05-14 19:58:45 +0300

    Draft elections display

--- src/app/elections-page/elections-page.component.ts
+++ src/app/elections-page/elections-page.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-elections-page',
   templateUrl: './elections-page.component.html',
   styleUrls: ['./elections-page.component.css']
 })
-export class ElectionsPageComponent implements OnInit {
+export class ElectionsPageComponent{
 
-  constructor() { }
+	elections_from_firestore: Observable<any[]>;
 
-  ngOnInit(): void {
+  constructor(private db: AngularFirestore) { 
+  	this.elections_from_firestore = this.db.collection('Elections').valueChanges();
   }
 
+  //elections_from_firestore = this.db.collection('Elections').valueChanges();
+	
+
+  print(){
+	  console.log("successful");
+	  console.log(this.elections_from_firestore);
+ 	}
 }
