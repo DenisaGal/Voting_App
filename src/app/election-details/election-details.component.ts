53e9f65f3bcfa67ac584f0913c250cc608f2aa55
DenisaGal <denisagal09@yahoo.com>
2021-05-16 20:00:22 +0300

    Added vote button and isActive function

--- src/app/election-details/election-details.component.ts
+++ src/app/election-details/election-details.component.ts
@@ -12,12 +12,12 @@ import { Observable } from 'rxjs';
 })
 export class ElectionDetailsComponent implements OnInit {
 
-	electionName: String = '';
-	electionDescription: String = '';
-	electionStart_Date: String = '';
-	electionStart_Time: String = '';
-	electionEnd_Date: String = '';
-	electionEnd_Time: String = '';
+	electionName: string = '';
+	electionDescription: string = '';
+	electionStart_Date: string = '';
+	electionStart_Time: string = '';
+	electionEnd_Date: string = '';
+	electionEnd_Time: string = '';
 	candidates_from_firestore: Observable<any[]>;
 
   constructor(private route: ActivatedRoute, private db: AngularFirestore) { 
@@ -35,9 +35,9 @@ export class ElectionDetailsComponent implements OnInit {
 	                                 				this.electionName = data[i].Name;
 	                                 				this.electionDescription = data[i].Description;
 	                                 				this.electionStart_Date = data[i].Start_Date;
-													this.electionStart_Time = data[i].Start_Time;
-													this.electionEnd_Date = data[i].End_Date;
-													this.electionEnd_Time = data[i].End_Time;
+																					this.electionStart_Time = data[i].Start_Time;
+																					this.electionEnd_Date = data[i].End_Date;
+																					this.electionEnd_Time = data[i].End_Time;
 	                                 			}
                                  			}
                                  });
@@ -46,5 +46,37 @@ export class ElectionDetailsComponent implements OnInit {
   }
 
   ngOnInit(): void { }
+
+  isActive(): boolean {
+  	//returns true if election can be voted for today
+  	let today:Date = new Date();
+  	//convert start date and time to timestamp type
+    let iStart_Date: Date = new Date(this.electionStart_Date);
+    let iStart_Time = this.electionStart_Time;
+    let sTime = iStart_Time.split(':', 2);
+    let Start_Timestamp = new Date(iStart_Date.getFullYear(), iStart_Date.getMonth(), iStart_Date.getDate(), +sTime[0], +sTime[1], 0o0, 0o0);
+    //convert end date and time to timestamp type
+    let iEnd_Date: Date = new Date(this.electionEnd_Date);
+    let iEnd_Time = this.electionEnd_Time;
+    let eTime = iEnd_Time.split(':', 2);
+    let End_Timestamp = new Date(iEnd_Date.getFullYear(), iEnd_Date.getMonth(), iEnd_Date.getDate(), +eTime[0], +eTime[1], 0o0, 0o0);
+
+    if(today.getTime() === Start_Timestamp.getTime() || today.getTime() === End_Timestamp.getTime()){
+    	return true;
+    }
+   	else if(today >= Start_Timestamp && today <= End_Timestamp){
+   		return true;
+   	}
+   	else{
+   		return false;
+   	}
+  	return false;
+  }
+
+  vote(): void{
+  	//to be implemented
+  	//make it so they can't vote multiple times!!!!
+  	window.alert("Yey you voted!");
+  }
 
 }
