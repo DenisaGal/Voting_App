d7151e63fbb053524dac3a1f68f25e31583177ba
ioana-lazar <62338641+ioana-lazar@users.noreply.github.com>
2021-05-21 12:39:09 +0300

    Cleanup

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -55,48 +55,6 @@ export class TopBarComponent implements OnInit {
      return TopBarComponent.isSignedIn;
   }
 
-   /*async getUserFromDb() : Promise<boolean>{
-     return new Promise<boolean>((resolve,reject)=>{
-            setTimeout(() => {
-                                this.db.collection<any>("Users").valueChanges()
-                                  .subscribe( data=>
-                                     {
-                                         let isAdmin: boolean = false;
-                                         var i;
-                                         for ( i = 0; i < data.length; i++){
-                                           var decrypted = CryptoJS.AES.decrypt(data[i].Email_Address.trim(), this.encPassword.trim()).toString(CryptoJS.enc.Utf8);
-                                           if(decrypted == UserProfileComponent.emailAddress){
-                                             if(data[i].Admin == true){
-                                                resolve(isAdmin);
-                                             }
-                                           }
-                                         }
-                                         reject("Not admin");
-                                     })
-            }, 1500);
-     }).then( b => {  return true; })
-     .catch((error) => { return false;});
-  }
-  isAdmin(): boolean {
-      return this.getUserFromDb().then( b => {  return true; }).catch((error) => { return false;});
-*/
-
-    /* this.auth.authState.subscribe(user => {
-           var current_user = user;
-           if(current_user != null){
-                var current_email = current_user.email;
-                var encrypted_email = CryptoJS.AES.encrypt(current_email!.trim(), this.encPassword.trim()).toString();
-                           var result = this.db.collection("Users", ref => ref.where("Email_Address", "==", encrypted_email).limit(1)).valueChanges()
-                            .subscribe(data=>{ console.log(data); });
-           }
-     });*/
-
-  isAdmin(): boolean {
-     if(UserProfileComponent.emailAddress == "denisagal09@yaho.com")
-        return true;
-     return false;
-  }
-
 
   public logout(): void {
     this.auth.signOut().then(() => {
